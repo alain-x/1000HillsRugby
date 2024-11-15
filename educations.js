8e1f57ca61bdce9672103f1cde4803291f4143d5
alain-x <alainvalentin04@gmail.com>
2024-11-15 10:40:00 +0200

    more files

---
+++ educations.js
@@ -0,0 +1,22 @@
+// Toggle Menu Visibility
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const openIcon = document.getElementById("menu-open-icon");
+const closeIcon = document.getElementById("menu-close-icon");
+
+menuToggle.addEventListener("change", function () {
+  if (this.checked) {
+    menu.classList.remove("hidden");
+    openIcon.classList.add("hidden");
+    closeIcon.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+    openIcon.classList.remove("hidden");
+    closeIcon.classList.add("hidden");
+  }
+});
+// Toggle Dropdown
+function toggleDropdown(menuId) {
+  const dropdown = document.getElementById(menuId);
+  dropdown.classList.toggle("hidden");
+}
