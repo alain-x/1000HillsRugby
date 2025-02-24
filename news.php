3dddcd9e246ec5a81a59fac1e908247b9b2cf77c
alain-x <alainvalentin04@gmail.com>
2025-02-24 11:41:39 +0200

    initial commit

--- news.php
+++ news.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:title" content="<?php echo $article['title']; ?>">
-<meta property="og:description" content="<?php echo $article['content']; ?>">
-<meta property="og:image" content="<?php echo $article['main_image_path']; ?>">
+    <meta property="og:description" content="<?php echo $article['content']; ?>">
+    <meta property="og:image" content="<?php echo $article['main_image_path']; ?>">
     <meta property="og:url" content="">
     <meta property="og:type" content="article">
     <title>Rugby News</title>
