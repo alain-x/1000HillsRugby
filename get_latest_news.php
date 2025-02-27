3e040e862bc804c87a01382fd4f0d982dc3acacc
alain-x <alainvalentin04@gmail.com>
2025-02-27 18:58:05 +0200

    initial commit

--- get_latest_news.php
+++ get_latest_news.php
@@ -1,31 +1,29 @@
 <?php
 header('Content-Type: application/json');
 
+// Database connection
 $servername = "localhost:3306";
 $username = "hillsrug_gasore";
 $password = "M00dle??";
 $dbname = "hillsrug_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Fetch the latest 5 articles
-$sql = "SELECT id, title, category, date_published, main_image_path, author 
-        FROM articles 
-        ORDER BY date_published DESC 
-        LIMIT 5";
+// Fetch the 5 latest news from database
+$sql = "SELECT id, title, subtitle, category, img, created_at FROM news ORDER BY created_at DESC LIMIT 5";
 $result = $conn->query($sql);
 
-$latestNews = [];
+$news = [];
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $latestNews[] = $row;
+        $news[] = $row;
     }
 }
 
 $conn->close();
 
-echo json_encode($latestNews);
+echo json_encode($news);
 ?>
