fba77e4772b0cb115c0fc725e65c564d52076fe5
alain-x <alainvalentin04@gmail.com>
2025-03-16 14:53:31 +0200

    initial commit

--- ecommerce-site/admin/inc/config.php
+++ ecommerce-site/admin/inc/config.php
@@ -1,40 +1,30 @@
 <?php
 // Error Reporting Turn On
-ini_set('error_reporting', E_ALL);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Setting up the time zone
 date_default_timezone_set('America/Los_Angeles');
 
-// Host Name
-$dbhost = 'localhost';
- // server
- 
-$dbname = 'hillsrug_ecommerceweb';
- 
-$dbuser = 'hillsrug_gasore';
- 
-$dbpass = 'M00dle??';
+// Database Configuration
+$dbhost = 'localhost'; // server
+$dbname = 'hillsrug_ecommerceweb'; // database name
+$dbuser = 'hillsrug_gasore'; // database username
+$dbpass = 'M00dle??'; // database password
 
 
-// Database Name
-//$dbname = 'ecommerceweb';
-
-// Database Username
-//$dbuser = 'root';
-
-// Database Password
-//$dbpass = '1234';
-
+// $dbname = 'ecommerceweb'; // database name
+// $dbuser = 'root'; // database username
+// $dbpass = '1234'; // database password
 // Defining base url
 define("BASE_URL", "https://www.1000hillsrugby.rw/");
 
 // Getting Admin url
-define("ADMIN_URL", BASE_URL . "admin" . "/");
+define("ADMIN_URL", BASE_URL . "admin/");
 
 try {
-	$pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch( PDOException $exception ) {
-	echo "Connection error :" . $exception->getMessage();
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
 }
