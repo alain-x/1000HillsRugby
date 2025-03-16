f703a675b2a8c07bdbaedc00ff7e357e1a78d8ae
alain-x <alainvalentin04@gmail.com>
2025-03-16 15:00:44 +0200

    inital commit

--- ecommerce-site/admin/inc/config.php
+++ ecommerce-site/admin/inc/config.php
@@ -1,7 +1,6 @@
 <?php
 // Error Reporting Turn On
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
 
 // Setting up the time zone
 date_default_timezone_set('America/Los_Angeles');
